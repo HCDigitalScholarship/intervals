68f6b1fc93ca0a7a454e8370e41068f168d744fd
Oleh Shostak <oleh.h.shostak@gmail.com>
2022-12-09 16:55:53 -0500

    Separate bar chart function

--- intervals/visualizations.py
+++ intervals/visualizations.py
@@ -151,6 +151,45 @@ def plot_ngrams_heatmap(ngrams_df, ngrams_duration=None, selected_patterns=[], v
                                             voices=voices)
     return _plot_ngrams_df_heatmap(processed_ngrams_df, heatmap_width=heatmap_width, heatmap_height=heatmap_height, includeCount=includeCount)
 
+def plot_ngrams_barchart(ngrams_df, ngrams_duration=None, selected_patterns=[], voices=[], chart_width=800,
+                        chart_height=300, includeCount=False):
+    """
+    Plot a bar chart for crim-intervals getNgram's output.
+    :param ngrams_df: crim-intervals getNgram's output
+    :param ngrams_duration: if not None, rely on durations in the
+    df to calculate the durations of the ngrams.
+    :param selected_patterns: list of specific patterns the users want (optional)
+    :param voices: list of specific voices the users want (optional)
+    :param chart_width: the width of the final bar chart (optional)
+    :param chart_height: the height of the final bar chart (optional)
+    :return: a bar chart that displays the different patterns and their counts
+    """
+    processed_ngrams_df = process_ngrams_df(ngrams_df, ngrams_duration=ngrams_duration,
+                                            selected_pattern=selected_patterns,
+                                            voices=voices)
+    return _plot_ngrams_df_barchart(processed_ngrams_df, chart_width=chart_width, chart_height=chart_height, includeCount=includeCount)
+
+def _plot_ngrams_df_barchart(processed_ngrams_df, chart_width=800, chart_height=300, includeCount=False):
+    """
+    Plot a bar chart for crim-intervals getNgram's processed output.
+    :param ngrams_df: processed crim-intervals getNgram's output.
+    :param selected_pattern: list of specific patterns the users want (optional)
+    :param voices: list of specific voices the users want (optional)
+    :param chart_width: the width of the final bar chart (optional)
+    :param chart_height: the height of the final bar chart (optional)
+    :return: a bar chart that displays the different patterns and their counts
+    """
+
+    processed_ngrams_df = processed_ngrams_df.dropna(how='any')
+    selector = alt.selection_multi(fields=['pattern'])
+    y = alt.Y("voice", sort=None)
+
+    # make a copy of the processed n_grams and turn them into Strings
+    new_processed_ngrams_df = processed_ngrams_df.copy()
+    new_processed_ngrams_df['pattern'] = processed_ngrams_df['pattern'].map(lambda cell: ", ".join(str(item) for item in cell), na_action='ignore')
+
+    variable = alt.X('pattern', axis=alt.Axis(labelAngle=-45))
+    return create_bar_chart(variable, 'count(pattern)', 'pattern', new_processed_ngrams_df, selector, selector)
 
 def _from_ema_to_offsets(df, ema_column):
     """

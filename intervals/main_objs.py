32da3daad0af6046dd1801ac8b992646c713c215
Richard Freedman <rfreedma@haverford.edu>
2023-03-03 20:38:27 -0500

    Update main_objs.py

--- intervals/main_objs.py
+++ intervals/main_objs.py
@@ -1749,7 +1749,8 @@ class ImportedPiece:
         cadence_type_dict = {"Clausula Vera": "o", "Abandoned Clausula Vera": "v", "Evaded Clausula Vera": "^",
                      "Authentic" : "<", "Evaded Authentic": ">", "Abandoned Authentic": "8", "Double Leading Tone" : "s",
                      "Evaded Double Leading Tone": "p", "Abandoned Double Leading Tone": "P", "Phrygian Clausula Vera": "d",
-                     "Altizans Only": "h", "Evaded Altizans Only": "H", "Leaping Contratenor": "X", "Reinterpreted": "D", "Phrygian": "d", "None": "*"}
+                     "Altizans Only": "h", "Evaded Altizans Only": "H", "Leaping Contratenor": "X", "Reinterpreted": "D", 
+                     "Phrygian": "d", "None": "*", "Quince": "Q"}
 
         # defining the default order list (or accepting the custom one)
         if customOrder == None:

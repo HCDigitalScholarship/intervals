abf32d1d98b23c2aa90465e402f84342c20dcfce
eleon024 <124386731+eleon024@users.noreply.github.com>
2023-09-21 16:31:41 -0400

    Update main_objs.py

--- crim_intervals/main_objs.py
+++ crim_intervals/main_objs.py
@@ -11,7 +11,7 @@ from more_itertools import consecutive_groups
 import os
 import re
 import requests
-import intervals
+import crim_intervals
 import collections
 import verovio
 import seaborn as sns
@@ -22,7 +22,7 @@ import matplotlib.lines as mlines
 import plotly.express as px
 from glob import glob
 from IPython.display import SVG, HTML
-cwd = os.path.dirname(intervals.__file__)
+cwd = os.path.dirname(crim_intervals.__file__)
 
 MEINSURI = 'http://www.music-encoding.org/ns/mei'
 MEINS = '{%s}' % MEINSURI

e98e9db6299403b52aa36166beca623bd46e8825
Richard Freedman <rfreedma@haverford.edu>
2025-03-07 21:55:15 +0100

    update model finder

--- crim_intervals/main_objs.py
+++ crim_intervals/main_objs.py
@@ -3749,7 +3749,9 @@ class CorpusBase:
         mass_mel = masses.batch(ImportedPiece.melodic, number_parts=False, metadata=False, kwargs={'df': mass_notes, 'kind': 'd', 'end': False})
         mass_entries = masses.batch(ImportedPiece.entries, number_parts=False, metadata=False,
                                     kwargs={'df': mass_mel, 'n': n, 'thematic': thematic, 'anywhere': anywhere})
-        res = pd.DataFrame(columns=list(model.metadata['title'] for model in models.scores), index=list(mass.metadata['title'] for mass in masses.scores))
+        model_name = model.metadata['composer'] + '_' + model.metadata['title']
+        mass_name = model.metadata['composer'] + '_' + model.metadata['title']
+        res = pd.DataFrame(columns=list(model_name for model in models.scores), index=list(mass_name for mass in masses.scores))
         res.columns.name = 'Model'
         res.index.name = 'Mass'
         for i, model in enumerate(models.scores):
@@ -3759,7 +3761,7 @@ class CorpusBase:
                 hits = stack[stack.isin(mod_patterns)]
                 if len(stack.index):
                     percent = len(hits.index) / len(stack.index)
-                    res.at[mass.metadata['title'], model.metadata['title']] = percent
+                    res.at[mass_name, model_name] = percent
         return res
 
     def derivativeAnalyzer(self, df=None, n=10):
